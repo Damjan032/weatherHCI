249342dae2230db99b9944e31bbd36c789a983b3
Damjan032 <damjan0032@gmail.com>
2020-03-31 14:37:59 +0200

    Dizajn

--- weather-ui/src/App.js
+++ weather-ui/src/App.js
@@ -1,24 +1,93 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id="header" className="fixed-top d-flex align-items-center header-transparent">
+        <div className="container d-flex align-items-center">
+
+          <div className="logo mr-auto">
+            <h1 className="text-light"><a href=""><span>WEATHER</span></a></h1>
+          </div>
+        </div>
       </header>
+
+
+      <section id="hero">
+        <div className="hero-container">
+          <h1>WEATHER STATISTIC</h1>
+          <h2></h2>
+          <a href="#about" className="btn-get-started scrollto">Get Started</a>
+        </div>
+      </section>
+
+
+
+
+      <section id="about" className="about">
+        <div className="container">
+          <div className="section-title">
+            <h2>OVDE CE SE DODAJE</h2>
+            <p>Tabela sa podacima</p>
+          </div>
+
+        </div>
+
+      </section>
+
+
+
+      <section id="services" className="services section-bg">
+        <div className="container">
+
+          <div className="section-title">
+            <h2>Tabelarni prikaz</h2>
+            <p>Tabela sa podacima</p>
+          </div>
+
+          <div className="row">
+
+          </div>
+
+        </div>
+      </section>
+
+
+
+      <section id="featured" className="featured">
+        <div className="container">
+          <div className="section-title">
+            <h2>Grafikon</h2>
+            <p>Tabela sa podacima</p>
+          </div>
+
+
+        </div>
+      </section>
+
+      <footer id="footer">
+
+        <div className="footer-top">
+
+          <div className="container">
+
+            <div className="row  justify-content-center">
+              <div className="col-lg-6">
+                <h3>weather</h3>
+                <p>Nesto pise.</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </footer>
+
+
+      <a href="#" className="back-to-top"><i className="icofont-simple-up"></i></a>
     </div>
   );
 }
